c9656f7504610028acf8030a32aa0e42cd22f8d1
Cahgnexx <cuonglvrepvn@gmail.com>
2018-09-05 23:16:38 +0700

    add broadcast

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-
+var port = process.env.PORT || 3000
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');  
 });
 
 io.on('connection', function(socket){
   console.log('a user connected');
-  console.log()
-})
+  socket.on('chat message', function(msg){    
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
+  });
+
+  socket.on('disconnect', function(){
+    console.log('user disconnect');
+  });
+});
 
 http.listen(3000, function(){
-  console.log('listening on *:3000');
+  console.log('listening on *:' + port);
 });
