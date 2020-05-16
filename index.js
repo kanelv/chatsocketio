2394041821588ac851548f6af48171fe4da9b7e5
Cuong Le <cuonglvrepvn@gmail.com>
2020-05-17 02:19:39 +0900

    add gitignore

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-#!/usr/bin/node
+#! /usr/bin/node
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
--- servermultiport.js
+++ servermultiport.js
@@ -1,19 +1,25 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var redis = require('socket.io-redis');
-io.adapter(redis({host:'localhost', port:6379}));
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const redis = require('socket.io-redis');
+const PORT = process.argv[2] || 3000
+// io.adapter(redis({host:'localhost', port:6379}));
 
-io.on('connection', (socket)=>{
-    io.of('/').adapter.clients((err, clients) => {
-      console.log(clients); // an array containing all connected socket ids
-    });
-    io.emit('hi', 'just add one connected into server');
+// io.on('connection', (socket)=>{
+//     io.of('/').adapter.clients((err, clients) => {
+//       console.log(clients); // an array containing all connected socket ids
+//     });
+//     io.emit('hi', 'just add one connected into server');
+// });
+
+app.get('/', (req, res) => {
+    res.status(200).send({
+        message: `Welcome to ${PORT}`});
 });
-http.listen(process.argv[2], (err)=>{
+http.listen(PORT, (err)=>{
     if(err){
         console.log(err);
     } else {
-        console.log('Sever now to listening at localhost:' + process.argv[2]);
+        console.log('Sever now to listening at localhost:' + PORT);
     }
 })
